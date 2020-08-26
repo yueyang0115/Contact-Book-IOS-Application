c7470ecc5db4aafa7052531b10b1db88794ebdab
yueyang <yueyang0115@outlook.com>
2020-08-25 23:28:10 -0400

    add UI layout and add/update/find function

--- HW1.playground/Contents.swift
+++ HW1.playground/Contents.swift
@@ -29,12 +29,17 @@ enum DukeProgram : String {
 
 // You can add code here
 
+// create DukePerson class
 class DukePerson : Person, CustomStringConvertible {
     var role : DukeRole = .Student
     var program :DukeProgram = .NA
     var description : String {
         let basicInfo : String = firstName + " " + lastName + " : a " + role.rawValue + " from " + whereFrom
         return basicInfo
+    }
+    
+    override init(){
+        super.init()
     }
     
     init(firstName: String, lastName: String, whereFrom: String, gender: Gender, role: DukeRole, program: DukeProgram)
@@ -49,7 +54,7 @@ class DukePerson : Person, CustomStringConvertible {
     }
 }
 
-// create DukePerson Array for test
+// use DukePerson Array as database for test
 var testPersons = [DukePerson]()
 let yueyang = DukePerson(firstName: "Yue", lastName: "Yang", whereFrom: "China", gender: .Female, role: .Student, program: .Grad)
 let ric = DukePerson(firstName: "Ric", lastName: "Telford", whereFrom: "Chatham County, NC", gender: .Male, role: .Professor, program: .NA)
@@ -65,8 +70,18 @@ for person in testPersons {
     print(person)
 }
 
-
+// views
 class HW1ViewController : UIViewController {
+    // self-defined variable
+    var firstNameLabel = UILabel()
+    var firstNameInput = UITextView()
+    var lastNameLabel = UILabel()
+    var lastNameInput = UITextView()
+    var fromWhereLabel = UILabel()
+    var fromWhereInput = UITextView()
+    let addButton = UIButton()
+    let findButton = UIButton()
+    
     override func loadView() {
 // You can change color scheme if you wish
         let view = UIView()
@@ -79,10 +94,90 @@ class HW1ViewController : UIViewController {
         self.view = view
         
 // You can add code here
-    
+        // layout settings
+        firstNameLabel.frame = CGRect(x: 50, y: 50, width: 100, height: 40)
+        firstNameLabel.text = "First Name:"
+        firstNameLabel.textColor = .white
+        view.addSubview(firstNameLabel)
+        
+        firstNameInput.frame = CGRect(x: 170, y: 60, width: 150, height: 20)
+        firstNameInput.textColor = .black
+        firstNameInput.backgroundColor = .white
+        firstNameInput.layer.cornerRadius = 5
+        view.addSubview(firstNameInput)
+        
+        lastNameLabel.frame = CGRect(x: 50, y: 80, width: 100, height: 40)
+        lastNameLabel.text = "Last Name:"
+        lastNameLabel.textColor = .white
+        view.addSubview(lastNameLabel)
+        view.addSubview(lastNameInput)
+        
+        lastNameInput.frame = CGRect(x: 170, y: 90, width: 150, height: 20)
+        lastNameInput.textColor = .black
+        lastNameInput.backgroundColor = .white
+        lastNameInput.layer.cornerRadius = 5
+        view.addSubview(lastNameInput)
+        
+        fromWhereLabel.frame = CGRect(x: 50, y: 110, width: 100, height: 40)
+        fromWhereLabel.text = "From:"
+        fromWhereLabel.textColor = .white
+        view.addSubview(fromWhereLabel)
+        
+        fromWhereInput.frame = CGRect(x: 170, y: 120, width: 150, height: 20)
+        fromWhereInput.textColor = .black
+        fromWhereInput.backgroundColor = .white
+        fromWhereInput.layer.cornerRadius = 5
+        view.addSubview(fromWhereInput)
+        
+        addButton.setTitle("Add/Update", for: .normal)
+        addButton.layer.cornerRadius = 10
+        addButton.setTitleColor(.black, for: .normal)
+        addButton.backgroundColor = .systemYellow
+        addButton.frame = CGRect(x: 50, y: 300, width: 120, height: 50)
+        addButton.addTarget(self, action: #selector(addPerson), for: .touchUpInside)
+        view.addSubview(addButton)
+        
+        findButton.setTitle("Find", for: .normal)
+        findButton.layer.cornerRadius = 10
+        findButton.setTitleColor(.black, for: .normal)
+        findButton.backgroundColor = .systemYellow
+        findButton.frame = CGRect(x: 200, y: 300, width: 120, height: 50)
+        findButton.addTarget(self, action: #selector(findPerson), for: .touchUpInside)
+        view.addSubview(findButton)
+        
     }
     
 // You can add code here
+    // button handler functions
+    @objc func addPerson() {
+        resignResponse()
+        for i in 0...testPersons.count-1{
+            if(testPersons[i].firstName == firstNameInput.text && testPersons[i].lastName == lastNameInput.text){
+                testPersons.remove(at: i)
+                break
+            }
+        }
+        let newPerson = DukePerson(firstName: firstNameInput.text, lastName: lastNameInput.text, whereFrom: fromWhereInput.text, gender: .Female, role: .Student, program: .Grad)
+        testPersons.append(newPerson)
+    }
+    
+    @objc func findPerson() {
+        resignResponse()
+        for person in testPersons{
+            if(person.firstName == firstNameInput.text && person.lastName == lastNameInput.text){
+                print(person)
+                return
+            }
+        }
+    }
+
+    func resignResponse(){
+        firstNameInput.resignFirstResponder()
+        lastNameInput.resignFirstResponder()
+        fromWhereInput.resignFirstResponder()
+    }
+    
+    
 
 }
 // Don't change the following line - it is what allowsthe view controller to show in the Live View window
