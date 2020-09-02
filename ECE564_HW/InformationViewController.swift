f53ae47f74a646b37b2fba9f554ca8299f3f60dd
yueyang <yueyang0115@outlook.com>
2020-09-02 00:42:05 -0400

    add stackViews

---
+++ ECE564_HW/InformationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InformationViewController.swift
+//  ECE564_HW
+//
+//  Created by yueyang on 9/1/20.
+//  Copyright © 2020 ECE564. All rights reserved.
+//
+
+import UIKit
+
+class InformationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
