cd811e1b53e94ab4f1e699628550c7420442e77c
yueyang <yueyang0115@outlook.com>
2020-10-02 00:15:25 -0400

    resize image

--- ECE564_HW/InformationViewController.swift
+++ ECE564_HW/InformationViewController.swift
@@ -198,7 +198,8 @@ class InformationViewController: UIViewController,UINavigationControllerDelegate
         //split hobbies and languages to [String]
         let hobbies: [String] = hobby.components(separatedBy: ",")
         let languages: [String] = language.components(separatedBy: ",")
-        let imageData: Data = userImage.image!.pngData()!
+        //var imageData: Data = userImage.image!.pngData()!
+        let imageData: Data = resizeImage(image: userImage.image!, targetSize: CGSize(width: 50, height: 50)).pngData()!
         // add person to database
         addPersonToDB(firstName: firstName, lastName: lastName, whereFrom: fromWhere, gender: gender, role: role, degree: degree, hobby: hobbies, language: languages, team: team, email: email, image: imageData)
                 
@@ -242,6 +243,33 @@ class InformationViewController: UIViewController,UINavigationControllerDelegate
     }
     
     // MARK: imagePicker-related
+    // resize image
+    func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+        let size = image.size
+
+        let widthRatio  = targetSize.width  / size.width
+        let heightRatio = targetSize.height / size.height
+
+        // Figure out what our orientation is, and use that to form the rectangle
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+        }
+
+        // This is the rect that we've calculated out and this is what is actually used below
+        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+
+        // Actually do the resizing to the rect using the ImageContext stuff
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return newImage!
+    }
+    
     func imagePickerController(_ picker: UIImagePickerController,
                                didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
